4d67f3290a0175718f0a055c48ba6edcecc49521
M-Moon <34864012+M-Moon@users.noreply.github.com>
2022-08-29 13:03:25 +0100

    Did some playing around with a basic useState hook to keep track of a counter that is rendered onto the page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Person = (props) => { // functional components > class-based
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last name: {props.lastName}</h2>
+    <h3>Age: {props.age}</h3>
+    </>
+  );
+};
+
+const App = () => {
+  const userName = 'Mark';
+  const otherName = 'Billy';
+
+  const [counter, setCounter] = useState(0); // array destructuring for state (HOOK!)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello React, my friend</h1>
+      <h2>yo</h2>
+      <Person name={userName} lastName={'Johnson'} age={30}/>
+      <Person 
+        name={otherName}
+        lastName='Jameson' 
+        age={45}
+      />
+      <div></div>
+      <button onClick={() => setCounter(counter-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    </div> // import / inject function with <Person />
   );
-}
+};
 
 export default App;
